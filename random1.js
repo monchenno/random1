a1250abf3678ec94f480295aeb7926c48e601ac7
monchenno <abmontero@yahoo.com>
2015-12-01 14:24:30 -0600

    random1 version 1

---
+++ random1.js
@@ -0,0 +1,8 @@
+//Math.random() devuelve numero aleatorio entre 0 y 1
+var numero = Math.random();
+var str = 'MAYOR que 0.5';
+if( numero <= 0.5){
+  str = 'MENOR que 0.5';
+}
+
+console.log('\n' + numero + str + '\n');
